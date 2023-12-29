3862250b3c874183c54e67fedcfee8df9d27fae2
Min Thant Mg <minthantmg@gicjp.com>
2023-12-29 13:11:50 +0900

    redirect home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Home from "@/app/home/page";
 
 const Page = () => {
     return (
         <div>
-
+            <div>
+                <Home />
+            </div>
         </div>
     );
 };
